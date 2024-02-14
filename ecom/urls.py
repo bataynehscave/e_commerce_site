dd29c9151490d3165e2b1080691b4fb92e74bce5
bataynehscave <mohdluai9@gmail.com>
2024-02-14 15:57:38 +0300

    added index view for products with search functionality including styling

--- ecom/urls.py
+++ ecom/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import include, path
+import shop.urls as shop_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('shop/', include(shop_urls)),
 ]
--- shop/models.py
+++ shop/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=300)
     category = models.CharField(max_length=50)
     price = models.FloatField()
     discount_price = models.FloatField()
     image = models.CharField(max_length=200)
+
+    def __str__(self) -> str :
+        return self.title
